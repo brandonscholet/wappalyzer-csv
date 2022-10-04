3072dff0ed44a9a5cf97ab314dc095a7f5af6ce2
Brandon Scholet <brandon@redsiege.com>
2022-10-04 10:58:40 -0500

    added timeouts to fix hanging thread issues

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     setup(
     scripts = ["src/wappy"],
     name='wappybird',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         dependencies
