2bcd0e67fb87344c4b76f32a2d6f3c0094acc143
Brandon Scholet <bscholet@gmail.com>
2023-01-18 09:57:37 -0500

    changed install deets to python3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     setup(
     scripts = ["src/wappy"],
     name='wappybird',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     install_requires=[
         dependencies
